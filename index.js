c58f9855ec5d4e12c20d209badcc0c32775877bc
vusause <mikevu123@gmail.com>
2017-02-27 13:34:22 -0500

    Changes to mailer, added hacker rank

--- index.js
+++ index.js
@@ -3,57 +3,140 @@
 // Import packages
 var Snoocore = require('snoocore');
 var postmark = require("postmark");
-// get this package before running
 var async = require('async');
+var hn = require('hack-news');
 
 
 // profiles emulate noSQL db using hash properties of JS objects
-// objects inserted should be of format {email: string, subreddits: array of strings, length: integer}
+// objects inserted should be of format {email: string, subreddits: array of strings, posts: integer, sort: string, hackernews: bool}
 var profiles = {};
 // this would be an insertion into db
-profiles[1] = {email: "mikevu123@gmail.com", subreddits: ["leagueoflegends", "uwaterloo", "nba"], length: 3};
-profiles[2] = {email: "vusause@hotmail.com", subreddits: ["anime", "redpill"], length: 5};
+profiles['1'] = {email: "mikevu123@gmail.com", subreddits: ["leagueoflegends", "uwaterloo", "nba"], posts: 3, sort: "hot", hackernews: true};
+profiles['2'] = {email: "vusause@hotmail.com", subreddits: ["anime", "TheRedPill"], posts: 5, sort: "new", hackernews: false};
 
 // Admin Email Client
 var client = new postmark.Client("");
 
 // Authentication for reddit API
 var reddit = new Snoocore({
-  userAgent: 'testing',
+  userAgent: '',
   oauth: { 
-    type: 'script',
+    type: '',
     key: '', 
     secret: '',
-    username: 'DankSeeEssMemer',
+    username: '',
     password: '',
     // make sure to set all the scopes you need
     scope: [ 'read' ]
   }
 });
 
-var subreddits = [];
-
 var message = "<ul>";
 
-subreddits.push("leagueoflegends");
-subreddits.push("uwaterloo");
+function mailer(to, posts) {
+    posts.forEach(function(post) {
+        message += "<li><a href='";
+        message += post.url;
+        message += "'>";
+        message += post.title;
+        message += "</a></li>";
+    });
+        
+    message += "</ul>";
+  
+    console.log(message);
+            
+    client.sendEmail({
+        "From": "mdhvu@uwaterloo.ca",
+        "To": to,
+        "Subject": "Daily Digest", 
+        "HtmlBody": message,
+        "TextBody": "Uh, oh something went wrong with this message!"
+    });
+            
+    message = "<ul>";
+}
 
-
-// reddit data used here temporarily
-// Similar to profiles should emulate noSQL db
-var redditData = [];
-
-function getSubPosts(sub, callback) {
+// not working mailer using postmarks templates
+function mailerTemplate(to, posts) {
     
-    reddit("/r/" + sub + "/hot").listing({limit: 3}).then(function(slice) {
-        slice.children.forEach(function(child) {
-            redditData.push({
-                title: child.data.title,
-                url: child.data.url,
-            });
+    var attach = [];
+    
+    posts.forEach(function(post) {
+        attach.push({
+            'url': post.url,
+            'title': post.title,
+            'attachment_size': "blank",
+            'attachment_type': "blank"
         });
-        console.log(redditData);
-        callback();
+    });
+    
+    console.log(attach);
+    
+    client.sendEmailWithTemplate({
+        "From": "mdhvu@uwaterloo.ca",
+        "To": to,
+        "TemplateId": ,
+        "TemplateModel": {
+        "body": "Daily Digest",
+        "attachment_details": attach,
+        "commenter_name": "Mike's Daily Digest",
+        "timestamp": "timestamp_Value",
+        "action_url": "action_url_Value",
+        "notifications_url": "notifications_url_Value"
+    }
+});
+}
+
+function addStories(ids, store, end, to) {
+    
+    var temp = store;
+    
+    async.each(ids, function(id, callback) {
+        hn.storyWithId(id).then(story => {
+            temp.push({
+                title: story.title,
+                url: story.url,
+            });
+            callback();
+        });
+    }, function (err) {
+        if (err) {
+            console.log("Uh, oh an error occurred");
+        }
+        mailerTemplate(to, temp);
+        console.log("reaches here");
+        end();
+    })
+}
+
+function getSubPosts(prof, callback) {
+    
+    var temp = [];
+    
+    async.each(prof.subreddits, function(sub, callback) {
+        reddit("/r/" + sub + "/" + prof.sort).listing({limit: prof.posts}).then(function(slice) {
+            slice.children.forEach(function(child) {
+                temp.push({
+                    title: child.data.title,
+                    url: child.data.url,
+                });
+            });
+            callback();
+        });
+    }, function (err) {
+        if (err) {
+            console.log("Uh, oh an error occurred");
+        } else {
+            if (prof.hackernews) {
+                hn.numberOfNewStories(prof.posts).then(stories => {
+                    addStories(stories, temp, callback, prof.email);
+                });
+            } else {
+                mailerTemplate(prof.email, temp);
+                callback();
+            }
+        }
     });
 }
 
@@ -63,30 +146,9 @@ exports.handler = (event, context, callback) => {
       
       if (err) {
           console.log("Uh, oh an error occurred");
-      } else {
-      
-      redditData.forEach(function(post) {
-        message += "<li><a href='";
-        message += post.url;
-        message += "'>";
-        message += post.title;
-        message += "</a></li>";
-      });
-        
-        message += "</ul>";
-  
-        console.log(message);
-
-        client.sendEmail({
-            "From": "mdhvu@uwaterloo.ca",
-            "To": "mikevu123@gmail.com",
-            "Subject": "Daily Digest", 
-            "HtmlBody": message,
-            "TextBody": "Uh, oh something went wrong with this message!"
-        });
-      }
+      } 
     }
   );
 
-  callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  callback(null, { message: 'Digest Ran!', event });
 };
